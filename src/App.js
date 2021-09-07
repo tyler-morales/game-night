ef8b3f9463283a116831a717020b977a0748a3ae
Tyler Morales <moratyle@gmail.com>
2021-09-06 21:28:56 -0500

    🛂  Register a new user

--- src/App.js
+++ src/App.js
@@ -1,86 +1,28 @@
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { SignUp } from './pages/SignUp'
+Amplify.configure(awsconfig)
 
 function App() {
   return (
-    <div className="bg-primary h-full pt-6 m-auto">
-      <header class="m-auto">
+    <div className="bg-primary min-h-screen py-6 m-auto">
+      <header className="m-auto">
         <img
           style={{ width: '150px' }}
-          class="m-auto"
+          className="m-auto"
           src={logo}
           alt="Game Night Logo"
         />
       </header>
-
-      <section class="grid gap-16 mt-9 w-11/12 m-auto">
-        <div>
-          <h1 class="font-bold text-center text-white text-3xl">
-            Create an Account
-          </h1>
-          <form class="mt-4 flex gap-4 flex-col">
-            <div class="flex gap-3 flex-col">
-              <label class="text-white text-xs" htmlFor="group-name">
-                Family Name
-              </label>
-              <input
-                type="text"
-                class="rounded-md py-3 pl-3"
-                placeholder="Family or Group name"
-              />
-            </div>
-            <div class="flex gap-3 flex-col">
-              <label class="text-white text-xs" htmlFor="email">
-                Email Addresss
-              </label>
-              <input
-                type="text"
-                class="rounded-md py-3 pl-3"
-                placeholder="Email Addresss"
-              />
-            </div>
-            <div class="flex gap-3 flex-col">
-              <label class="text-white text-xs" htmlFor="password">
-                Password
-              </label>
-              <input
-                type="text"
-                class="rounded-md py-3 pl-3"
-                placeholder="Password"
-              />
-            </div>
-            <input
-              class="rounded-md bg-tertiary py-3 mt-6 cursor-pointer"
-              type="submit"
-            />
-          </form>
-          <h4 class="text-white text-center mt-5">
-            Have an Account?
-            <span class="font-body text-quad cursor-pointer font-bold pl-1">
-              Log in
-            </span>
-          </h4>
-        </div>
-        <div>
-          <h2 class="text-white text-2xl">
-            Track and view every family game night.
-          </h2>
-          <ul>
-            <li class="text-white font-body">
-              <span class="font-bold"> Document EVERY game</span>. Easily track
-              each game your family plays.
-            </li>
-            <li class="text-white font-body">
-              <span class="font-bold"> Discover new insights</span>. Uncover who
-              has the most wins overall wins.
-            </li>
-            <li class="text-white font-body">
-              <span class="font-bold"> Digitize your boardgame</span> catalogue.
-              Catagorize & store your board games.
-            </li>
-          </ul>
-        </div>
-      </section>
+      <Router>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/" />
+        </Switch>
+      </Router>
     </div>
   )
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+import { Auth } from 'aws-amplify'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
 const schema = yup.object().shape({
-  groupName: yup.string().min(4).max(20).required(),
+  username: yup.string().min(4).max(20).required(),
   email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  password: yup.string().min(8).required(),
 })
 
 export const SignUp = () => {
@@ -19,108 +20,131 @@ export const SignUp = () => {
     resolver: yupResolver(schema),
   })
 
-  const onSubmit = (data) => console.log(data)
+  const [serverError, setServerError] = useState({ status: false, message: '' })
+
+  const onSubmit = async (data) => {
+    console.log(data, data.username)
+    try {
+      const { user } = await Auth.signUp({
+        username: data.username,
+        password: data.password,
+        attributes: {
+          email: data.email,
+        },
+      })
+      console.log(user)
+    } catch (error) {
+      console.error('error signing up:', error)
+      setServerError({ status: true, message: error.message })
+    }
+  }
 
   const renderError = (message) => message
 
   return (
-    <section class="flex flex-col justify-center md:flex-row-reverse gap-16 mt-9 md:mt-14 w-11/12 lg:max-w-screen-xl m-auto">
-      <div class="md:w-1/2 lg:max-w-sm">
-        <h1 class="font-bold text-center text-white text-3xl md:text-left">
+    <section className="flex flex-col justify-center md:flex-row-reverse gap-16 mt-9 md:mt-14 w-11/12 lg:max-w-screen-xl m-auto">
+      <div className="md:w-1/2 lg:max-w-sm">
+        <h1 className="font-bold text-center text-white text-3xl md:text-left">
           Create an Account
         </h1>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          class="mt-4 flex gap-2 flex-col"
+          className="mt-4 flex gap-2 flex-col"
         >
-          <div class="flex gap-3 flex-col">
-            <label class="text-white text-xs" htmlFor="groupName">
+          <div className="flex gap-3 flex-col">
+            <label className="text-white text-xs" htmlFor="username">
               Family/ Friend Group Name
             </label>
             <input
+              value="test1234"
               type="text"
-              {...register('groupName')}
-              class={
-                errors.groupName
+              {...register('username')}
+              className={
+                errors.username || serverError
                   ? 'focus:ring-error transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
                   : 'focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
               }
               placeholder="ahslandboys2000"
             />
-            <p class="text-error text-sm">
-              {errors.groupName &&
+            <p className="text-error text-sm">
+              {errors.username &&
                 renderError('Must be between 1-20 alpha-numerical characters')}
+              {serverError.message}
             </p>
           </div>
-          <div class="flex gap-3 flex-col">
-            <label class="text-white text-xs" htmlFor="email">
+          <div className="flex gap-3 flex-col">
+            <label className="text-white text-xs" htmlFor="email">
               Email Addresss
             </label>
             <input
+              value="moratyle@gmail.com"
               type="email"
               {...register('email')}
-              class={
+              className={
                 errors.email
                   ? 'focus:ring-error transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
                   : 'focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
               }
               placeholder="anjay@gmail.com"
             />
-            <p class="text-error text-sm">
+            <p className="text-error text-sm">
               {errors.email && renderError('Must be a valid email address')}
             </p>
           </div>
-          <div class="flex gap-3 flex-col">
-            <label class="text-white text-xs" htmlFor="password">
+          <div className="flex gap-3 flex-col">
+            <label className="text-white text-xs" htmlFor="password">
               Password
             </label>
             <input
+              value="qwerty123"
               type="text"
               {...register('password')}
-              class={
+              className={
                 errors.password
                   ? 'focus:ring-error transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
                   : 'focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2'
               }
               placeholder="qwerty123"
             />
-            <p class="text-error text-sm">
+            <p className="text-error text-sm">
               {errors.password &&
-                renderError('Must be at least 6 characters long ')}
+                renderError(
+                  'Must be at least 8 characters long and include a number'
+                )}
             </p>
           </div>
           <input
-            class="transition-all transform hover:translate-y-1 rounded-md bg-tertiary py-3 mt-6 cursor-pointer border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-tertiary focus:border-transparent"
+            className="transition-all transform hover:translate-y-1 rounded-md bg-tertiary py-3 mt-6 cursor-pointer border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-tertiary focus:border-transparent"
             type="submit"
             value="Create Account"
           />
         </form>
-        <h4 class="text-white text-center mt-5">
+        <h4 className="text-white text-center mt-5">
           Have an Account?
           <Link
             to="/login"
-            class="font-body text-quad cursor-pointer rounded-md font-bold px-2  border-transparent focus:outline-none focus:ring-1 focus:ring-quad focus:border-transparent"
+            className="font-body text-quad cursor-pointer rounded-md font-bold px-2  border-transparent focus:outline-none focus:ring-1 focus:ring-quad focus:border-transparent"
           >
             Log in
           </Link>
         </h4>
       </div>
-      <div class="md:w-1/2 lg:max-w-sm">
-        <h2 class="text-white text-2xl mb-7">
+      <div className="md:w-1/2 lg:max-w-sm">
+        <h2 className="text-white text-2xl mb-7">
           Track and view every family game night.
         </h2>
         <ul>
-          <li class="text-white font-body mb-5">
-            <span class="font-bold"> Document EVERY game</span>. Easily track
-            each game your family plays.
+          <li className="text-white font-body mb-5">
+            <span className="font-bold"> Document EVERY game</span>. Easily
+            track each game your family plays.
           </li>
-          <li class="text-white font-body mb-5">
-            <span class="font-bold"> Discover new insights</span>. Uncover who
-            has the most wins overall wins.
+          <li className="text-white font-body mb-5">
+            <span className="font-bold"> Discover new insights</span>. Uncover
+            who has the most wins overall wins.
           </li>
-          <li class="text-white font-body">
-            <span class="font-bold"> Digitize your boardgame</span> catalogue.
-            Catagorize & store your board games.
+          <li className="text-white font-body">
+            <span className="font-bold"> Digitize your boardgame</span>{' '}
+            catalogue. Catagorize & store your board games.
           </li>
         </ul>
       </div>
