749cda51986894b73832db9f147633bfef198215
Tyler Morales <moratyle@gmail.com>
2021-09-09 11:43:35 -0500

    💫 Delete menu animations

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -53,7 +53,7 @@ export const DashboardNav = () => {
         <div className="w-full">
           <Link
             to="dashboard"
-            className="tranition-all duration-150 md:rounded-md ease-in-out md:border-none border-b-2 border-darkGreen py-4 px-3 md:px-8 items-center text-lg justify-self-start flex gap-2 w-full hover:bg-darkGreen"
+            className="md:rounded-md ease-in-out md:border-none border-b-2 border-darkGreen py-4 px-3 md:px-8 items-center text-lg justify-self-start flex gap-2 w-full hover:bg-darkGreen"
           >
             <RiBarChart2Fill />
             <span>Dashboard</span>
