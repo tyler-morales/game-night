8aeb0ca17b53a252dbf4963f797a93f8a8c9c3aa
Tyler Morales <moratyle@gmail.com>
2021-09-15 13:14:37 -0500

    🚧  Add temp avatar image

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -32,7 +32,11 @@ function Profile() {
           {/* Account Info */}
           <div>
             <h2 className="text-white text-2xl text-left mb-5">Account Info</h2>
-            <div className="flex flex-col gap-3 bg-primary p-5 text-left rounded-lg">
+            <div className="flex flex-col gap-3 bg-primary p-5 text-left rounded-lg shadow-lg">
+              {/* TODO: Add dyamic avatar */}
+              <div className="flex justify-center items-center bg-quad text-center rounded-full h-28 w-28 m-auto mb-4">
+                <span>{user.username.substring(0, 2).toUpperCase()}</span>
+              </div>
               <h3 className="text-white text-base">
                 Account Name: {user.username}
               </h3>
@@ -43,25 +47,31 @@ function Profile() {
           {/* Members */}
           <div>
             <h2 className="text-white text-2xl text-left mb-5">Members</h2>
-            <div className="flex justify-between bg-primary p-5 text-left rounded-lg border-2 border-white">
+            <div className="flex justify-between bg-primary p-5 text-left rounded-lg border-2 border-white shadow-lg">
               <h3 className="text-white text-base">Tyler</h3>
               <div className="flex gap-3">
                 <FiTool className="cursor-pointer" size=".75em" />
                 <RiDeleteBinLine className="cursor-pointer" size=".75em" />
               </div>
             </div>
+            <span className="cursor-pointer text-xs uppercase mt-6 block">
+              + Add Group Member
+            </span>
           </div>
 
           {/* Games */}
           <div>
             <h2 className="text-white text-2xl text-left mb-5">Your Games</h2>
-            <div className="flex justify-between bg-primary p-5 text-left rounded-lg border-2 border-white">
+            <div className="flex justify-between bg-primary p-5 text-left rounded-lg border-2 border-white shadow-lg">
               <h3 className="text-white text-base">Dominoes</h3>
               <div className="flex gap-3">
                 <FiTool className="cursor-pointer" size=".75em" />
                 <RiDeleteBinLine className="cursor-pointer" size=".75em" />
               </div>
             </div>
+            <span className="cursor-pointer text-xs uppercase mt-6 block">
+              + Add a new Game
+            </span>
           </div>
         </div>
       )}
