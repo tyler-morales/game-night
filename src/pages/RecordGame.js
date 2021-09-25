fc054cb16f695d3b7b4252b72d101899e4876c48
Tyler Morales <moratyle@gmail.com>
2021-09-24 21:35:09 -0500

    🐛  Add checkboxes Match the checkbox UI to the state in Formik

--- src/pages/RecordGame.js
+++ src/pages/RecordGame.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Formik, Field, Form } from 'formik'
+
 import { Dashboard } from '../layout/Dashboard'
 
 import { LoadingRipple } from '../components/loadingIndicator/LoadingRipple'
@@ -10,38 +12,20 @@ import useUser from '../hooks/useUser'
 import protectedRoute from './protectedRoute'
 
 function RecordGame() {
-  const dummyPlayers = [
-    { name: 'Pupa' },
-    { name: 'Korvo' },
-    { name: 'Jesse' },
-    { name: 'Terry' },
-    { name: 'Gobblins' },
-  ]
   const [loading, updateLoading] = useState(true)
 
   const { members } = useLoadMembers(updateLoading)
   const { games } = useLoadGames(updateLoading)
   const { user } = useUser()
 
-  const [checkedState, setCheckedState] = useState(
-    new Array(dummyPlayers.length).fill(false)
-  )
+  const [checkedStatus, setCheckedStatus] = useState(members.map(() => false))
 
-  let playingPlayers = []
-  for (var index in checkedState) {
-    if (checkedState[index] === true) {
-      playingPlayers.push(dummyPlayers[index])
-    }
+  const handleSetChecked = async (index) => {
+    let newState = [...checkedStatus] // make a copy of the current state of the checkedStatus
+    newState[index] = !checkedStatus[index] // update the relevant index based on the previous state. If its checked, uncheck it and vice versa.
+    setCheckedStatus(newState)
   }
-
-  console.log(members, playingPlayers)
-
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((player, index) =>
-      index === position ? !player : player
-    )
-    setCheckedState(updatedCheckedState)
-  }
+  console.log(checkedStatus)
 
   // Rendered elements
   const gamesOptions = games.map((game, index) => (
@@ -50,21 +34,24 @@ function RecordGame() {
     </option>
   ))
 
-  const playerCheckboxes = dummyPlayers.map((player, index) => {
+  const playerCheckboxes = members.map((player, index) => {
     return (
-      <div key={index} className="flex gap-2">
+      <div
+        key={index}
+        className="rounded-md ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 flex gap-2"
+      >
         <label htmlFor={player.name} className="cursor-pointer">
-          <input
+          <Field
             type="checkbox"
+            onClick={() => handleSetChecked(index)}
             id={player.name}
-            name={player.name}
-            checked={checkedState[index]}
-            onChange={() => handleOnChange(index)}
+            name="players"
+            value={player.name}
             className="opacity-0 cursor-pointer hidden"
           />
           <span
-            className={`py-1 px-2 rounded-md transition-all border-2 border-quad text-base ${
-              checkedState[index] ? 'bg-quad text-primary' : 'bg-transparent'
+            className={`block h-full py-1 px-2 rounded-md transition-all border-2 border-quad text-base ${
+              checkedStatus[index] ? 'bg-quad text-primary' : 'bg-transparent'
             }`}
           >
             {player.name}
@@ -81,58 +68,81 @@ function RecordGame() {
         <div>Loading...</div>
       ) : (
         <div className="mt-8 flex flex-col gap-3 bg-primary p-5 md:p-8 text-left rounded-lg shadow-lg w-full md:w-96 m-auto">
-          <form className="flex flex-col gap-4">
-            {/* Game Played */}
-            <div className="flex flex-col gap-3">
-              <label className="text-lg text-left" htmlFor="game-select">
-                What did you play?
-              </label>
-              <select
-                className="ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 mt-3 bg-quad rounded-md text-base text-primary py-2 px-4"
-                name="games"
-                id="game-select"
+          <Formik
+            initialValues={{
+              gamePlayed: '',
+              players: [],
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                setSubmitting(false)
+              }, 400)
+            }}
+          >
+            <Form className="flex flex-col gap-4">
+              {/* Game Played */}
+              <div className="flex flex-col gap-3">
+                <label className="text-lg text-left" htmlFor="game-select">
+                  What did you play?
+                </label>
+                <Field
+                  component="select"
+                  className="ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 mt-3 bg-quad rounded-md text-base text-primary py-2 px-4"
+                  name="gamePlayed"
+                  id="game-select"
+                >
+                  <option value="">
+                    {loading ? 'Loading' : 'Select Game'}
+                  </option>
+                  {gamesOptions}
+                </Field>
+              </div>
+
+              {/* Game Players */}
+              <div>
+                <label className=" text-lg text-left" htmlFor="players">
+                  Who Played?
+                </label>
+                <div
+                  role="group"
+                  aria-labelledby="checkbox-group"
+                  className="flex flex-wrap gap-4 mt-3"
+                >
+                  {loading ? <LoadingRipple /> : playerCheckboxes}
+                </div>
+              </div>
+
+              {/* Winners */}
+              <div>
+                <label className=" text-lg text-left" htmlFor="winner">
+                  Who Won?
+                </label>
+                <div className="flex wrap gap-4 mt-3">
+                  <div className="flex gap-2">
+                    <input type="checkbox" id="pupa" name="pupa" />
+                    <label className="text-base" htmlFor="pupa">
+                      Pupa
+                    </label>
+                  </div>
+                  <div className="flex gap-2">
+                    <input type="checkbox" id="korvo" name="korvo" />
+                    <label className="text-base" htmlFor="korvo">
+                      Korvo
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              {/* Submit */}
+              <button
+                type="submit"
+                className="w-full transition-all ring-offset-primary ring-offset-2 focus:ring-tertiary focus:outline-none focus:ring-2 text-lg rounded-md py-2 px-4 text-primary bg-tertiary mt-5"
               >
-                <option value="">{loading ? 'Loading' : 'Select Game'}</option>
-                {gamesOptions}
-              </select>
-            </div>
-
-            {/* Game Players */}
-            <div>
-              <label className=" text-lg text-left" htmlFor="players">
-                Who Played?
-              </label>
-              <div className="flex flex-wrap gap-4 mt-3">
-                {loading ? <LoadingRipple /> : playerCheckboxes}
-              </div>
-            </div>
-
-            {/* Winners */}
-            <div>
-              <label className=" text-lg text-left" htmlFor="winner">
-                Who Won?
-              </label>
-              <div className="flex wrap gap-4 mt-3">
-                <div className="flex gap-2">
-                  <input type="checkbox" id="pupa" name="pupa" />
-                  <label className="text-base" htmlFor="pupa">
-                    Pupa
-                  </label>
-                </div>
-                <div className="flex gap-2">
-                  <input type="checkbox" id="korvo" name="korvo" />
-                  <label className="text-base" htmlFor="korvo">
-                    Korvo
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            {/* Submit */}
-            <button className="w-full transition-all ring-offset-primary ring-offset-2 focus:ring-tertiary focus:outline-none focus:ring-2 text-lg rounded-md py-2 px-4 text-primary bg-tertiary mt-5">
-              Record Game
-            </button>
-          </form>
+                Record Game
+              </button>
+            </Form>
+          </Formik>
         </div>
       )}
     </Dashboard>
