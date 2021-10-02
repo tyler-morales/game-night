370c6fcf5b18fe25cd11c58cd7ecd22d6790a7d1
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2021-10-01 22:44:51 -0500

    ✨ Feature/create leaderboard (#17)
    * 📦  Install Recharts
    
    * 📱  Critical fix to make table responsive
    
    * 🏗️  Scaffold out Chart component for dashboard
    Layout the Chart component block
    
    * 🚧  Dynamically set chart data
    
    * ✨  Connect option value to chart gameId data
    
    * 🐛  Match select state to chart game state
    
    * 💄  Make list emoji icon available for signup only
    
    * 💄  Style legend

---
+++ src/components/dashboard/Chart.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import { API } from 'aws-amplify'
+
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+import { listWins } from '../../graphql/queries'
+
+export const Chart = (gameId) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchMemberWins()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameId])
+
+  const fetchMemberWins = async () => {
+    try {
+      let filteredWins = await API.graphql({
+        query: listWins,
+        variables: {
+          filter: {
+            gameId: { contains: gameId.value },
+          },
+        },
+      })
+
+      let filteredWinsByGame = filteredWins.data.listWins.items
+
+      filteredWinsByGame = filteredWinsByGame.map((item) => {
+        const winnerObject = {
+          name: item.member.name,
+          Wins: item.wins,
+        }
+        return winnerObject
+      })
+
+      setData(filteredWinsByGame)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        data={data}
+        margin={{
+          top: 0,
+          right: 5,
+          left: -25,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" style={{ fill: '#fff', fontSize: 20 }} />
+        <YAxis style={{ fill: '#fff', fontSize: 20 }} />
+        <Tooltip />
+        <Legend wrapperStyle={{ fontSize: '20px' }} />
+        <Bar dataKey="Wins" fill="#5cd5dd" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
