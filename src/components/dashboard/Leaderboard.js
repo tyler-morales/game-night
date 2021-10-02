aaaed50529e24422917c2447a1cced3fd860f778
Tyler Morales <moratyle@gmail.com>
2021-10-01 22:11:36 -0500

    🐛  Match select state to chart game state

--- src/components/dashboard/Leaderboard.js
+++ src/components/dashboard/Leaderboard.js
@@ -11,13 +11,19 @@ import useLoading from '../../hooks/useLoading'
 
 export const Leaderboard = () => {
   const { loading, dataLoaded } = useLoading()
-  const [selectGameOption, setSelectGameOption] = useState('')
-
   const { games } = useLoadGames(dataLoaded)
+  const [selectGameOption, setSelectGameOption] = useState(null)
+  const [optionName, setOptionName] = useState()
+
+  useEffect(() => {
+    fetchMemberWins()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [games])
 
   const Options = () => {
     const handleChange = (e) => {
       setSelectGameOption(e.target.value)
+      setOptionName(e.target.value)
     }
     return (
       <select
@@ -25,6 +31,7 @@ export const Leaderboard = () => {
         className="ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 bg-quad rounded-md text-base text-primary ml-8 px-2 py-1 w-52"
         name="gamePlayed"
         id="game-select"
+        value={optionName}
       >
         {games.map((game, index) => {
           return (
@@ -37,41 +44,41 @@ export const Leaderboard = () => {
     )
   }
 
-  useEffect(() => {
-    fetchMemberWins()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
   const fetchMemberWins = async () => {
     try {
       dataLoaded()
+      // set the initial select option to the first game in the users's games
+      if (!loading) setSelectGameOption(games[0].id)
+      if (!loading) setOptionName(games[0].name)
     } catch (err) {
       console.error(err)
     }
   }
 
   return (
-    <DashboardItemContainer title="Leaderboard" options={<Options />}>
-      {!loading ? (
-        <>
-          <div className="md:h-80 flex flex-col gap-6">
-            {listMembers.length > 0 ? (
-              <Chart value={selectGameOption} />
-            ) : (
-              <div className="flex flex-col gap-4 bg-primary rounded-lg p-8 ">
-                <h4 className="text-2xl border-b-2 border-quad pb-4">
-                  You haven't played any games to update the Leaderboard
-                </h4>
-                <p className="text-sm">
-                  💡 Click the Record a Game button to record your first game!
-                </p>
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <LoadingRipple />
-      )}
-    </DashboardItemContainer>
+    <>
+      <DashboardItemContainer title="Leaderboard" options={<Options />}>
+        {!loading ? (
+          <>
+            <div className="md:h-80 flex flex-col gap-6">
+              {listMembers.length > 0 ? (
+                <Chart value={selectGameOption} />
+              ) : (
+                <div className="flex flex-col gap-4 bg-primary rounded-lg p-8 ">
+                  <h4 className="text-2xl border-b-2 border-quad pb-4">
+                    You haven't played any games to update the Leaderboard
+                  </h4>
+                  <p className="text-sm">
+                    💡 Click the Record a Game button to record your first game!
+                  </p>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <LoadingRipple />
+        )}
+      </DashboardItemContainer>
+    </>
   )
 }
