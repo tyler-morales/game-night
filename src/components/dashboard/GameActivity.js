67195b792a4d5f21f209ac10f9a0b81d5f3c8caa
Tyler Morales <moratyle@gmail.com>
2021-10-04 21:36:45 -0500

    📱  Make Game Activity tile responsive

--- src/components/dashboard/GameActivity.js
+++ src/components/dashboard/GameActivity.js
@@ -6,7 +6,7 @@ import { LoadingRipple } from '../loadingIndicator/LoadingRipple'
 import useGetRecords from '../../hooks/useGetRecords'
 import { ChartHeatmap } from '../charts/ChartHeatmap'
 
-export const Heatmap = () => {
+export const GameActivity = () => {
   const [dateRecords, setDateRecords] = useState(0)
   const { data, loading } = useGetRecords()
 
@@ -52,7 +52,7 @@ export const Heatmap = () => {
     return (
       <select
         onChange={handleChange}
-        className="ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 bg-quad rounded-md text-base text-primary ml-8 px-2 py-1 w-52"
+        className="ring-offset-primary ring-offset-2 focus:ring-quad focus:outline-none focus:ring-2 bg-quad rounded-md text-base text-primary px-2 py-1 w-full md:w-52"
         name="heatMapDateSelection"
         id="heatmap-date-select"
         value={optionName}
@@ -69,10 +69,9 @@ export const Heatmap = () => {
         <div>
           <div className="md:h-80 flex flex-col gap-6">
             {dateRecords.length > 0 ? (
-              <>
+              <div className="card">
                 <ChartHeatmap period={selectOption} data={dateRecords} />
-                <hr className="border-1 border-quad" />
-              </>
+              </div>
             ) : (
               <div className="flex flex-col gap-4 bg-primary rounded-lg p-8 ">
                 <h4 className="text-2xl border-b-2 border-quad pb-4">
--- src/components/dashboard/Grid.js
+++ src/components/dashboard/Grid.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { GameDistribution } from './GameDistribution'
-import { Heatmap } from './Heatmap'
+import { GameActivity } from './GameActivity'
 import { History } from './History'
 import { Leaderboard } from './Leaderboard'
 
 export const Grid = () => {
   return (
-    <div className="flex flex-col gap-10">
-      <div className="grid xl:grid-cols-2 gap-10">
-        <Leaderboard />
+    <div className="flex flex-col gap-4 md:gap-10">
+      <div className="grid xl:grid-cols-2 gap-4 md:gap-10">
+        {/* <Leaderboard /> */}
         <History />
       </div>
-      <div className="grid xl:grid-cols-3 gap-10">
-        <GameDistribution />
-        <div className="xl:col-span-2">
-          <Heatmap />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-10">
+        {/* <GameDistribution /> */}
+        <div className="col-span-1 lg:col-span-2">
+          <GameActivity />
         </div>
       </div>
     </div>
