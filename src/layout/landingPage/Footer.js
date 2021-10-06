5ed0d124154604261c1cbd71a4b7e0acf2a30951
Tyler Morales <moratyle@gmail.com>
2021-10-05 22:55:20 -0500

    💄  Design Footer

---
+++ src/layout/landingPage/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="text-quad text-sm text-center pt-16 md:pt-28 pb-8">
+      Designed and Developed by Tyler Morales
+    </footer>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Nav } from '../layout/landingPage/Nav'
 import { Hero } from '../layout/landingPage/Hero'
 import { About } from '../layout/landingPage/About'
 import { Features } from '../layout/landingPage/Features'
+import { Footer } from '../layout/landingPage/Footer'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Hero />
       <About />
       <Features />
+      <Footer />
     </Container>
   )
 }
