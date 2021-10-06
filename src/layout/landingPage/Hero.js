58427d45cb353453ed4ade15be88cdcfbcf82cbd
Tyler Morales <moratyle@gmail.com>
2021-10-06 11:45:32 -0500

    🐛  Fix bug Animate CTA so as to not have it appear first on page load

--- src/layout/landingPage/Hero.js
+++ src/layout/landingPage/Hero.js
@@ -18,7 +18,6 @@ export const Hero = () => {
       >
         Taking the hassle out of game night
       </h2>
-
       <img
         data-aos="fade-up"
         data-aos-delay="350"
@@ -26,15 +25,17 @@ export const Hero = () => {
         src={HeroImage}
         alt="Dashboard Preview"
       />
-      <h3 className="text-white font-light font-body text-center text-base md:text-lg mt-14 lg:mt-20">
-        Ready to upgrade your game ledger?
-      </h3>
-      <NavLink
-        to="/dashboard"
-        className="text-lg py-2 px-4 bg-secondary text-primary rounded-md mt-4 md:mt-8 block w-max m-auto text-center"
-      >
-        <span>Create an Account</span>
-      </NavLink>
+      <div data-aos="fade-in">
+        <h3 className="text-white font-light font-body text-center text-base md:text-lg mt-14 lg:mt-20">
+          Ready to upgrade your game ledger?
+        </h3>
+        <NavLink
+          to="/dashboard"
+          className="text-lg py-2 px-4 bg-secondary text-primary rounded-md mt-4 md:mt-8 block w-max m-auto text-center"
+        >
+          <span>Create an Account</span>
+        </NavLink>
+      </div>
     </header>
   )
 }
