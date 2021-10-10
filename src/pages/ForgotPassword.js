b19d970efeddaf3e97c05af72f0d15865e543b94
Tyler Morales <moratyle@gmail.com>
2021-10-09 22:11:36 -0500

    💄  Style and refactor focus

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -71,7 +71,7 @@ const StepOne = (props) => {
   }
 
   return (
-    <div className="flex flex-col gap-10 md:flex-row-reverse">
+    <div className="flex flex-col w-11/12 m-auto justify-center mt-9 md:mt-14 py-6 md:max-w-md">
       <section className="md:w-full flex gap-4 flex-col">
         <Formik
           validationSchema={SignUpStepOneSchema}
@@ -81,60 +81,31 @@ const StepOne = (props) => {
           {({ errors, touched }) => (
             <Form className="flex gap-4 flex-col">
               <h1 className="font-bold text-white text-3xl md:text-left">
-                Create an Account
+                Reset Password
               </h1>
               <div className="flex gap-3 flex-col">
                 <label className="text-white text-xs" htmlFor="username">
-                  Family/ Friend Group Name
+                  Account name
                 </label>
                 <Field
                   name="username"
                   type="text"
-                  className="focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2"
-                  placeholder="ahslandboys2000"
+                  className="transition-all rounded-md py-3 pl-3 border-2 focus-tertiary-ring"
+                  placeholder="username"
+                  autofocus={true}
                 />
-                {serverError && (
+                {/* {serverError && (
                   <span className="text-error">{serverError}</span>
                 )}
                 {errors.username && touched.username ? (
                   <span className="text-sm text-error">{errors.username}</span>
-                ) : null}
-              </div>
-
-              <div className="flex gap-3 flex-col">
-                <label className="text-white text-xs" htmlFor="email">
-                  Email Addresss
-                </label>
-                <Field
-                  type="email"
-                  name="email"
-                  className="focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2"
-                  placeholder="anjay@gmail.com"
-                />
-                {errors.email && touched.email ? (
-                  <span className="text-sm text-error">{errors.email}</span>
-                ) : null}
-              </div>
-
-              <div className="flex gap-3 flex-col">
-                <label className="text-white text-xs" htmlFor="password">
-                  Password
-                </label>
-                <Field
-                  type="text"
-                  name="password"
-                  className="focus:ring-tertiary transition-all rounded-md py-3 pl-3 border-2 focus:outline-none focus:ring-2"
-                  placeholder="qwerty123"
-                />
-                {errors.password && touched.password ? (
-                  <span className="text-sm text-error">{errors.password}</span>
-                ) : null}
+                ) : null} */}
               </div>
 
               <button
                 type="submit"
                 disabled={signingIn ? true : false}
-                className={`transition-all transform hover:translate-y-1 w-full rounded-md bg-tertiary py-3 mt-6 cursor-pointer border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-tertiary focus:border-transparent ${
+                className={`transition-all transform hover:translate-y-1 w-full rounded-md bg-tertiary py-3 mt-6 cursor-pointer border-2 border-transparent focus-tertiary-ring ${
                   signingIn ? 'opacity-50 cursor-wait' : 'opacity-100'
                 }`}
               >
@@ -147,40 +118,12 @@ const StepOne = (props) => {
         {/* Already have an account */}
         <div className="mt-6">
           <p className="font-body text-white">
-            <span className="mr-2">Already have an account?</span>
+            <span className="mr-2">Remembered your password?</span>
             <NavLink to="/sign-in">
               <span className="text-quad cursor-pointer">Sign In</span>
             </NavLink>
           </p>
         </div>
-      </section>
-
-      {/* Marketing Section */}
-      <section className="md:w-full ">
-        <h2 className="text-white text-2xl mb-7">
-          Track and view every family game night.
-        </h2>
-        <ul className="grid gap-5">
-          <li className="grid gap-2 grid-cols-list">
-            <span>✍️</span>
-            <span className="text-white font-body font-bold">
-              Document EVERY game. Easily track each game your family plays.
-            </span>
-          </li>
-          <li className="grid gap-2 grid-cols-list">
-            <span>📊</span>
-            <span className="text-white font-body font-bold">
-              Discover new insights. Uncover who has the most wins overall wins.
-            </span>
-          </li>
-          <li className="grid gap-2 grid-cols-list">
-            <span>📚</span>
-            <span className="text-white font-body font-bold">
-              Digitize your boardgame catalogue. Catagorize & store your board
-              games.
-            </span>
-          </li>
-        </ul>
       </section>
     </div>
   )
