3fc5966ea4956de52d92bc5dd2e9d6fde55f3846
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2021-10-21 14:04:45 -0600

    ⭐  Feature/radar chart (#49)
    * 🚧  Render Radar Chart
    
    * 💄 Edit UI of radar chart
    
    * ✨  Add data to radar chart
    
    * 💄 Finish styling Game activity
    
    * 🗑️  Erase console logs

--- src/components/charts/ChartHeatmap.js
+++ src/components/charts/ChartHeatmap.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import HeatMap from '@uiw/react-heat-map'
 import useLoadSpecficRecords from '../../hooks/useLoadSpecficRecords'
+import { ChartRadar } from './ChartRadar'
 
 export const ChartHeatmap = ({ period, data }) => {
   // Get date info
@@ -11,8 +12,6 @@ export const ChartHeatmap = ({ period, data }) => {
   const [endDate, setEndDate] = useState()
   const [selected, setSelected] = useState(`${year}/${month + 1}/${day}`)
   const { games } = useLoadSpecficRecords(selected)
-
-  // console.log(selected)
 
   useEffect(() => {
     // Year to date of Current year
@@ -40,7 +39,7 @@ export const ChartHeatmap = ({ period, data }) => {
     const today = `${year}/${month + 1}/${day}`
 
     return (
-      <div className="text-left text-base">
+      <div className="text-left text-base overflow-scroll md:h-52">
         <span>
           On <span className="font-bold">{date ? date : today}</span>
         </span>
@@ -49,7 +48,7 @@ export const ChartHeatmap = ({ period, data }) => {
           <span className="font-bold">{count ? count : '0'} </span>
           {count === 1 ? 'game' : 'games'}
         </span>
-        <ul className="mt-2">
+        <ul className="mt-2 md:pb-16">
           {games.map((item, index) => {
             return (
               <li className="mb-2" key={index}>
@@ -69,7 +68,7 @@ export const ChartHeatmap = ({ period, data }) => {
                     )
                   })}
                 </ul>
-                <ul>
+                <ul className="border-b-2 pb-2 border-dotted border-quad">
                   <span>
                     <span className="mr-2">🏆</span> Winner(s):{' '}
                   </span>
@@ -92,15 +91,6 @@ export const ChartHeatmap = ({ period, data }) => {
     )
   }
 
-  const Tooltip = () => {
-    return (
-      <div className="bg-white">
-        <span>0 games on </span>
-        <span>date</span>
-      </div>
-    )
-  }
-
   return (
     <div className="flex overflow-hidden items-end flex-col w-full">
       <HeatMap
@@ -108,10 +98,11 @@ export const ChartHeatmap = ({ period, data }) => {
         style={{ color: 'white' }}
         startDate={new Date(startDate)}
         endDate={new Date(endDate)}
-        legendCellSize={20}
+        legendCellSize={0}
         rectSize={10}
         width={675}
-        legendRender={(props) => <rect {...props} y={props.y + 5} rx="2" />}
+        height={120}
+        legendRender={0}
         rectProps={{
           rx: 2,
         }}
@@ -138,14 +129,7 @@ export const ChartHeatmap = ({ period, data }) => {
 
       <hr className="border-1 border-white w-full" />
 
-      {/* TODO: Add members and filter below data by member */}
-      {/* <div className="self-start mt-4 mb-6 flex gap-3">
-        <span className="text-sm border-2 border-quad rounded-lg py-2 px-3">
-          MoralesFam
-        </span>
-      </div> */}
-
-      <div className="pt-4 grid grid-cols-1 sm:grid-cols-2 self-start w-full min-h-170">
+      <div className="pt-4 grid grid-cols-1 sm:grid-cols-2 self-start w-full md:h-52">
         <div className="border-b sm:border-b-0 sm:border-r border-white pb-4 sm:pb-0 sm:pr-4">
           <h3 className="text-sm uppercase font-bold text-left mb-4 ">
             Game Activity Stats
@@ -154,7 +138,10 @@ export const ChartHeatmap = ({ period, data }) => {
         </div>
 
         <div className="pt-4 sm:pt-0 sm:pl-4">
-          <h3 className="text-sm uppercase font-bold text-left">Wins</h3>
+          <h3 className="text-sm uppercase font-bold text-left mb-4">
+            Active days of the week
+          </h3>
+          <ChartRadar />
         </div>
       </div>
     </div>
