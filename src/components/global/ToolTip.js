ec85a9c77ef0b9d7926490f3bc14117c888fdd66
Tyler Morales <moratyle@gmail.com>
2021-11-07 22:20:31 -0700

    ✨ Add a position prop Controls placement of tooltip

--- src/components/global/ToolTip.js
+++ src/components/global/ToolTip.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-export const ToolTipContent = ({ info }) => {
+export const ToolTipContent = ({ info, position }) => {
+  console.log(position)
   const [iconToggle, setIconToggle] = useState(false)
   const { icon, title, description } = info
 
@@ -12,7 +13,11 @@ export const ToolTipContent = ({ info }) => {
     <div className="relative" onClick={handleIconToggle}>
       <span className="cursor-pointer">{icon}</span>
       {iconToggle && (
-        <div className="absolute bottom-9 md:bottom-11 -right-8 md:right-auto bg-white py-3 px-5 w-64 rounded-md border-2 boxShadow-base">
+        <div
+          className={`absolute z-50 -right-8 md:right-auto bg-white py-3 px-5 w-64 rounded-md border-2 boxShadow-base ${
+            position === 'bottom' ? 'bottom-9 md:bottom-11' : 'top-9 md:top-11'
+          }`}
+        >
           <h3 className="text-primary text-sm mb-1 font-bold ">{title}</h3>
           <p className="text-primary text-sm z-10">{description}</p>
         </div>
