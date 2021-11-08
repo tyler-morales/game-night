bc6ac4ad8febedf1eeef9af48ae5dbfc56ef8c58
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2021-11-08 16:10:24 -0700

    ✨ Redirect user to homepage (#61)
    Redirect to hompage if user clicks logo in the auth process

--- src/components/nav/AuthNav.js
+++ src/components/nav/AuthNav.js
@@ -1,13 +1,20 @@
 import logo from '../../logo.svg'
+import { useHistory } from 'react-router-dom'
 
 export const AuthNav = () => {
+  let history = useHistory()
+
+  const redirectHome = () => {
+    history.push('/')
+  }
   return (
     <header className="m-auto md:my-10">
       <img
         style={{ width: '150px' }}
-        className="m-auto"
+        className="m-auto cursor-pointer"
         src={logo}
         alt="Game Night Logo"
+        onClick={redirectHome}
       />
     </header>
   )
