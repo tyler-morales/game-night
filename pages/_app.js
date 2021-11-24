8db422040b80c0e2d6e07fb75b7eddd7a53159c9
tyler-morales <tyler.morales.dev@gmail.com>
2021-11-23 18:15:20 -0600

    💡  Add comments to import statements

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
 import { useEffect } from 'react'
 
+// 💻 AWS
 import Amplify from 'aws-amplify'
 import awsconfig from '../aws-exports'
 
+// 🎨 CSS
 import '../styles/globals.css'
 import 'react-day-picker/lib/style.css'
 import '../styles/dayPicker.css'
 
+// 📚 Library
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
+// 👥 Authentication
 import { UserProvider } from '../contexts/UserContext'
 
-Amplify.configure(awsconfig)
+Amplify.configure({ ...awsconfig, ssr: true })
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
